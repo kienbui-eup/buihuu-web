77b60be7307d7f5ea8edc5f88853a335d68debed
vibiz <kienbd@eupgroup.net>
2026-09-04 23:37:35 +0700

    Trang chủ: ghim tư liệu dòng họ năm Bính Ngọ

--- src/views/GrampsjsViewRecentBlogPosts.js
+++ src/views/GrampsjsViewRecentBlogPosts.js
@@ -4,6 +4,19 @@ import {GrampsjsConnectedComponent} from '../components/GrampsjsConnectedCompone
 import '../components/GrampsjsSearchResultList.js'
 import {fireEvent} from '../util.js'
 
+const FEATURED_POSTS = [
+  {
+    id: 'SBHNC22',
+    title: 'Bài phát biểu ngày khánh thành nhà thờ họ Bùi Hữu',
+    description: 'Lịch sử dòng họ và quá trình trùng tu nhà thờ tổ năm 2026',
+  },
+  {
+    id: 'SBHNC23',
+    title: 'Văn khấn giỗ tổ năm Bính Ngọ',
+    description: 'Văn khấn ngày 24 tháng Giêng, tức 12/03/2026',
+  },
+]
+
 export class GrampsjsViewRecentBlogPosts extends GrampsjsConnectedComponent {
   static get styles() {
     return [
@@ -16,54 +29,97 @@ export class GrampsjsViewRecentBlogPosts extends GrampsjsConnectedComponent {
         }
 
         h3 {
-          margin: 0 0 15px;
+          margin: 0 0 12px;
           font-size: 24px;
+        }
+        h4 {
+          margin: 18px 0 6px;
+          color: var(--heritage-ink);
+          font: 600 15px/1.5 var(--grampsjs-heading-font-family);
+        }
+        ul {
+          list-style: none;
+          margin: 0;
+          padding: 0;
+        }
+        li {
+          border-top: 1px solid var(--heritage-rule);
+          padding: 10px 0;
+        }
+        li:first-child {
+          border-top: 0;
+        }
+        a.post {
+          display: block;
+          min-height: 44px;
+          font: 600 16px/1.5 var(--grampsjs-heading-font-family);
+          color: var(--md-sys-color-primary);
+          text-decoration: none;
+        }
+        a.post:hover {
+          text-decoration: underline;
+          text-underline-offset: 4px;
+        }
+        a.post small {
+          display: block;
+          font: 400 13px/1.6 var(--grampsjs-body-font-family);
+          color: var(--md-sys-color-on-surface-variant);
+        }
+        p.more {
+          margin: 12px 0 0;
+          font-size: 0.95em;
         }
       `,
     ]
   }
 
   renderContent() {
-    if (!this._data?.data?.length) {
-      return html`
-        <h3>${this._('Latest Blog Post')}</h3>
-        <p>
-          Chưa có bài viết. Những câu chuyện và kỷ niệm của dòng họ sẽ được lưu
-          lại tại đây.
-        </p>
-        ${this.appState.permissions.canEdit
-          ? html`<a href="/blog">${this._('Blog')}</a>`
-          : ''}
-      `
-    }
+    const featuredIds = new Set(FEATURED_POSTS.map(post => post.id))
+    const recentPost = this._data?.data?.find(
+      post => !featuredIds.has(post.gramps_id)
+    )
+
     return html`
-      <h3>${this._('Latest Blog Post')}</h3>
-      <grampsjs-search-result-list
-        large
-        selectable
-        @search-result:clicked="${this._handleClick}"
-        .data="${this._data.data.slice(0, 1).map(obj => ({
-          object: obj,
-          object_type: 'source',
-        }))}"
-        .appState="${this.appState}"
-        date
-        noSep
-      >
-      </grampsjs-search-result-list>
+      <h3>Tư liệu năm Bính Ngọ</h3>
+      <ul>
+        ${FEATURED_POSTS.map(
+          post => html`<li>
+            <a class="post" href="/blog/${post.id}"
+              >${post.title}<small>${post.description}</small></a
+            >
+          </li>`
+        )}
+      </ul>
+      ${recentPost
+        ? html`<h4>Mới cập nhật</h4>
+            <grampsjs-search-result-list
+              large
+              selectable
+              @search-result:clicked="${this._handleClick}"
+              .data="${[{object: recentPost, object_type: 'source'}]}"
+              .appState="${this.appState}"
+              date
+              noSep
+            >
+            </grampsjs-search-result-list>`
+        : ''}
+      <p class="more"><a href="/blog">Mở kho sử</a></p>
     `
   }
 
   renderLoading() {
     return html`
-      <h3>${this._('Latest Blog Post')}</h3>
-      <grampsjs-search-result-list
-        .data="${this._data.data}"
-        .appState="${this.appState}"
-        loading
-        numberLoading="1"
-        noSep
-      ></grampsjs-search-result-list>
+      <h3>Tư liệu năm Bính Ngọ</h3>
+      <ul>
+        ${FEATURED_POSTS.map(
+          post => html`<li>
+            <a class="post" href="/blog/${post.id}"
+              >${post.title}<small>${post.description}</small></a
+            >
+          </li>`
+        )}
+      </ul>
+      <p class="more"><a href="/blog">Mở kho sử</a></p>
     `
   }
 
@@ -85,7 +141,7 @@ export class GrampsjsViewRecentBlogPosts extends GrampsjsConnectedComponent {
     }
     return `/api/sources/?rules=${encodeURIComponent(
       JSON.stringify(rules)
-    )}&pagesize=1&sort=-change&locale=${
+    )}&pagesize=3&sort=-change&locale=${
       this.appState.i18n.lang || 'en'
     }&profile=all&extend=all`
   }
